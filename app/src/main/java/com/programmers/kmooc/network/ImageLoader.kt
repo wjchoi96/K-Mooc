53f50c1fcaf38de9a415e271553391496237a10e
bridgecwj <cwj@bridge.co>
2022-05-13 23:23:52 +0900

    [feat] image loader 캐시 처리

--- app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
+++ app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Handler
 import android.os.Looper
+import androidx.collection.LruCache
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -13,9 +14,11 @@ import java.net.URL
 import kotlin.concurrent.thread
 
 object ImageLoader {
-
+    private var memoryCache : LruCache<String, Bitmap>? = null
+    private val handler = Handler(Looper.getMainLooper())
     fun loadImage(url: String, completed: (Bitmap?) -> Unit) {
-        val handler = Handler(Looper.getMainLooper())
+        initCache()
+
         if(getBitmap(url) != null){
             completed.invoke(getBitmap(url))
             return
@@ -25,7 +28,7 @@ object ImageLoader {
             var bitmap: Bitmap?
             try{
                 bitmap = BitmapFactory.decodeStream(URL(url).openConnection().getInputStream())
-                saveBitmap(bitmap)
+                saveBitmap(url, bitmap)
             }catch (e: Exception){
                 e.printStackTrace()
                 bitmap = null
@@ -33,10 +36,21 @@ object ImageLoader {
             handler.post { completed.invoke(bitmap) }
         }
     }
-    private fun saveBitmap(bitmap: Bitmap?){
-
+    private fun initCache(){
+        if(memoryCache != null)
+            return
+        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+        val cacheSize = maxMemory/8
+        memoryCache = object : LruCache<String, Bitmap>(cacheSize){
+            override fun sizeOf(key: String, value: Bitmap): Int {
+                return value.byteCount/1024
+            }
+        }
+    }
+    private fun saveBitmap(url: String, bitmap: Bitmap?){
+        memoryCache?.put(url, bitmap ?: return)
     }
     private fun getBitmap(url: String): Bitmap? {
-        return null
+        return memoryCache!![url]
     }
 }
