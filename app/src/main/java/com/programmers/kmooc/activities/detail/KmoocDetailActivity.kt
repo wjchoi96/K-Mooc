af278b0607c21bab79abc2bb7397e3483f7bb70e
bridgecwj <cwj@bridge.co>
2022-05-13 22:45:56 +0900

    [feat] deatil view, view model 추가
     - view model은 필요한 데이터를 repository에게 요청해서 받아오고 livedata를 옵져빙 하고 있는 뷰에게 livedata에 값을 갱신해서 필요한 data, event들을 갱신시켜준다
     - view는 view model을 옵져빙한 채 대기하다가 data, event가 발생하면 적절한 동작을 행한다

--- app/src/main/java/com/programmers/kmooc/activities/detail/KmoocDetailActivity.kt
+++ app/src/main/java/com/programmers/kmooc/activities/detail/KmoocDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.programmers.kmooc.activities.detail
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.programmers.kmooc.KmoocApplication
@@ -8,6 +10,7 @@ import com.programmers.kmooc.databinding.ActivityKmookDetailBinding
 import com.programmers.kmooc.models.Lecture
 import com.programmers.kmooc.network.ImageLoader
 import com.programmers.kmooc.utils.DateUtil
+import com.programmers.kmooc.utils.showToast
 import com.programmers.kmooc.utils.toVisibility
 import com.programmers.kmooc.viewmodels.KmoocDetailViewModel
 import com.programmers.kmooc.viewmodels.KmoocDetailViewModelFactory
@@ -21,15 +24,72 @@ class KmoocDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityKmookDetailBinding
     private lateinit var viewModel: KmoocDetailViewModel
 
+    private var courseId : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initData()
+        binding = ActivityKmookDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        setUpViewModel()
+        viewModel.fetchDetail(courseId)
+    }
 
+    private fun initData(){
         val kmoocRepository = (application as KmoocApplication).kmoocRepository
         viewModel = ViewModelProvider(this, KmoocDetailViewModelFactory(kmoocRepository)).get(
             KmoocDetailViewModel::class.java
         )
-
-        binding = ActivityKmookDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        courseId = intent.getStringExtra(INTENT_PARAM_COURSE_ID)
+        if(courseId.isNullOrBlank()){
+            showToast("key 값이 유실되었습니다")
+            finish()
+        }
     }
+
+    private fun initView(){
+        binding.toolbar.setNavigationOnClickListener { viewModel.backClick() }
+    }
+
+    private fun setUpViewModel(){
+        viewModel.dataLoading .observe(this){
+            binding.progressBar.visibility = it.toVisibility()
+        }
+
+        viewModel.showToastEvent.observe(this){
+            showToast(it)
+        }
+
+        viewModel.finishViewEvent.observe(this) {
+            if(!it.isNullOrBlank()) showToast(it)
+            finish()
+        }
+
+        viewModel.lecture.observe(this) {
+            setView(it)
+        }
+    }
+
+    private fun setView(lecture: Lecture){
+        loadImage(lecture.courseImageLarge, binding.lectureImage)
+        binding.toolbar.title = lecture.name
+        binding.lectureNumber.setDescription("• 강좌번호 ::", lecture.number)
+        binding.lectureType.setDescription("• 강좌분류 :::", lecture.classfyName)
+        binding.lectureOrg.setDescription("• 운영기관 ::", lecture.orgName)
+        binding.lectureTeachers.setDescription("• 교수정보 ::", lecture.teachers ?: "")
+        val dateStr = "${DateUtil.formatDate(lecture.start)} ~ ${DateUtil.formatDate(lecture.end)}"
+        binding.lectureDue.setDescription("• 운영기간 :::", dateStr)
+        if(!lecture.overview.isNullOrBlank()){
+            binding.webView.settings.javaScriptEnabled = true
+            binding.webView.loadData(lecture.overview, "text/html; charset=utf-8", "UTF-8")
+        }
+    }
+
+    private fun loadImage(urlStr : String, into : ImageView){
+        ImageLoader.loadImage(urlStr){
+            it?.let { bitmap -> into.setImageBitmap(bitmap) }
+        }
+    }
+
 }
--- app/src/main/java/com/programmers/kmooc/viewmodels/KmoocDetailViewModel.kt
+++ app/src/main/java/com/programmers/kmooc/viewmodels/KmoocDetailViewModel.kt
@@ -1,15 +1,44 @@
 package com.programmers.kmooc.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.programmers.kmooc.models.Lecture
 import com.programmers.kmooc.repositories.KmoocRepository
 
 
 class KmoocDetailViewModel(private val repository: KmoocRepository) : BaseViewModel() {
+    /**
+     * live data for data
+     */
+    private val _lecture: MutableLiveData<Lecture> = MutableLiveData()
+    val lecture: LiveData<Lecture> = _lecture
 
-    fun detail(courseId: String) {
-        repository.detail(courseId) { lecture ->
+    /**
+     * live data for event
+     */
+    private val _finishViewEvent: MutableLiveData<String> = MutableLiveData()
+    val finishViewEvent: LiveData<String> = _finishViewEvent
+
+    fun fetchDetail(courseId: String?) {
+        if(courseId.isNullOrBlank()){
+            _finishViewEvent.value = "정보가 유실되었습니다"
+            return
         }
+        if(loading){
+            setRefreshLoading(false)
+            return
+        }
+        setDataLoading(true)
+        repository.detail(courseId) {
+            setDataLoading(false)
+            _lecture.postValue(it)
+        }
+    }
+
+    fun backClick(){
+        _finishViewEvent.value = ""
     }
 }
 
