56671723d06150e22f65b5d674df684090f417b4
bridgecwj <cwj@bridge.co>
2022-05-13 22:28:54 +0900

    [feat] lectures adapter 추가
     - view holder view 설정 코드 추가
     - 현재 item size 상태 변수 추가

--- app/src/main/java/com/programmers/kmooc/activities/list/LecturesAdapter.kt
+++ app/src/main/java/com/programmers/kmooc/activities/list/LecturesAdapter.kt
@@ -2,15 +2,20 @@ package com.programmers.kmooc.activities.list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.programmers.kmooc.R
 import com.programmers.kmooc.databinding.ViewKmookListItemBinding
 import com.programmers.kmooc.models.Lecture
+import com.programmers.kmooc.network.ImageLoader
+import com.programmers.kmooc.utils.DateUtil
 
 class LecturesAdapter : RecyclerView.Adapter<LectureViewHolder>() {
 
     private val lectures = mutableListOf<Lecture>()
     var onClick: (Lecture) -> Unit = {}
+    val currentItemSize: Int
+        get() = lectures.size
 
     fun updateLectures(lectures: List<Lecture>) {
         this.lectures.clear()
@@ -32,8 +37,25 @@ class LecturesAdapter : RecyclerView.Adapter<LectureViewHolder>() {
     override fun onBindViewHolder(holder: LectureViewHolder, position: Int) {
         val lecture = lectures[position]
         holder.itemView.setOnClickListener { onClick(lecture) }
+        holder.bind(lecture)
     }
 }
 
-class LectureViewHolder(binding: ViewKmookListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+class LectureViewHolder(
+    private val binding: ViewKmookListItemBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(lecture: Lecture){
+        binding.lectureTitle.text = lecture.name
+        binding.lectureFrom.text = lecture.classfyName
+        val dateStr = "${DateUtil.formatDate(lecture.start)} ~ ${DateUtil.formatDate(lecture.end)}"
+        binding.lectureDuration.text = dateStr
+        loadImage(lecture.courseImage, binding.lectureImage)
+    }
+
+    private fun loadImage(urlStr : String, into : ImageView) {
+        ImageLoader.loadImage(urlStr) {
+            it?.let { bitmap -> into.setImageBitmap(bitmap) }
+        }
+    }
 }
