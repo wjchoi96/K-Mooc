fa3a42f8605249d44ea25de65013c4d9cf41e748
bridgecwj <cwj@bridge.co>
2022-05-13 22:54:15 +0900

    [feat] image loader 클래스에 image load 코드 추가

--- app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
+++ app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
@@ -2,15 +2,41 @@ package com.programmers.kmooc.network
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Handler
+import android.os.Looper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 import java.net.URL
+import kotlin.concurrent.thread
 
 object ImageLoader {
+
     fun loadImage(url: String, completed: (Bitmap?) -> Unit) {
-        //TODO: String -> Bitmap 을 구현하세요
+        val handler = Handler(Looper.getMainLooper())
+        if(getBitmap(url) != null){
+            completed.invoke(getBitmap(url))
+            return
+        }
+
+        thread(start = true) {
+            var bitmap: Bitmap?
+            try{
+                bitmap = BitmapFactory.decodeStream(URL(url).openConnection().getInputStream())
+                saveBitmap(bitmap)
+            }catch (e: Exception){
+                e.printStackTrace()
+                bitmap = null
+            }
+            handler.post { completed.invoke(bitmap) }
+        }
+    }
+    private fun saveBitmap(bitmap: Bitmap?){
+
+    }
+    private fun getBitmap(url: String): Bitmap? {
+        return null
     }
 }
